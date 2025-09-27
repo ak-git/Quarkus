acc02fa0c37500bd1f01dd49d3682a55b52e3b1a
Alexander Kobelev <ak.mail.ru@gmail.com>
2025-09-27 16:16:36 +0300

    Add logger config

--- quarkus/src/main/java/com/ak/quarkus/GreetingResource.java
+++ quarkus/src/main/java/com/ak/quarkus/GreetingResource.java
@@ -4,15 +4,20 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @Path("/hello")
 public class GreetingResource {
+  private static final Logger LOGGER = Logger.getLogger(GreetingResource.class);
+
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
-    return "Hello World! %s".formatted(ZonedDateTime.now(ZoneId.systemDefault()));
+    String message = "Hello World! %s".formatted(ZonedDateTime.now(ZoneId.systemDefault()));
+    LOGGER.debug(message);
+    return message;
   }
 }
